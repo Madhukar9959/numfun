e16d5eba14ec18846c5f23a0b5205353709acf55
Madhukar9959 <madhusende9959@gmail.com>
2024-08-22 15:17:41 +0530

    Added na.rm argument

--- R/mean.R
+++ R/mean.R
@@ -1,6 +1,7 @@
-#' Title  Find the mean
+#' This function calculates the mean of a numeric vector with custom options.
 #'
 #' @param x any character vector
+#' @param na.rm Logical; should missing values be removed?
 #'
 #' @return a numeric number
 #' @export
@@ -8,6 +9,6 @@
 #' @examples
 #' x <- c(1,2,3,4,5)
 #' mean_custom(x)
-mean_custom <- function(x) {
+mean_custom <- function(x, na.rm=FALSE) {
   sum(x) / length(x)
 }
